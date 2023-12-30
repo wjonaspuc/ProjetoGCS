6f3f8b391d3ff1b3275d9e5e17d5db48b1377702
Denivaldo Correia <denivaldo.caldas@gmail.com>
2023-12-30 02:44:59 -0300

    Codificação da tela de consultas já agendadas.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        System.out.println("CONSULTAS AGENDADAS PARA O MÊS DE JANEIRO DE 2024:");
+        System.out.println("MÉDICO: Fulano Souza    -  PACIENTE: Sicrano Silva - DIA 10/01/2024 às 10:00");
+        System.out.println("MÉDICO: Beltrano Santos -  PACIENTE: John Doe      - DIA 15/01/2024 às 14:00");
+        System.out.println();
+        
     }
 }
 
