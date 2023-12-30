63b5afbc7ad88bdc0d9d445955a8ec716d768e62
Denivaldo Correia <denivaldo.caldas@gmail.com>
2023-12-30 03:42:12 -0300

    Codificação da tela de prontuário/histórico.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("PRONTUÁRIO");
+        System.out.println("PACIENTE: Sicrano da Silva");
+        System.out.println("Faz uso de medicamento contínuo: NÃO; Fumante: NÃO; Bebidas alcóolicas: NÃO.");
+        System.out.println("SINTOMAS: Coceira e Descamação nas mãos.");
+        System.out.println("EXAMES REALIZADOS: Teste de alergia ( Prick). Resultado: Negativo.");
+        System.out.println("Medicação Receitada: Pomada uso tópico 3x ao dia.");
 
     }
 }
